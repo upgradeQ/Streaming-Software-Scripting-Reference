795a06ea216372b7ad4e627f6651c86312417db0
upgradeQ <18361586+upgradeQ@users.noreply.github.com>
2024-02-06 23:47:19 +0300

    Fix stream key, add comment about autoconfig

--- src/stream_key.py
+++ src/stream_key.py
@@ -17,9 +17,11 @@ def script_update(settings):
 
 
 def callback(props, prop):
+    # https://github.com/obsproject/obs-studio/blob/66978c4d23e32d0f67ec5ac7246a8fea01bf4740/UI/window-basic-auto-config.cpp#L1097
     service = S.obs_frontend_get_streaming_service()
     settings = S.obs_service_get_settings(service)
     S.obs_data_set_string(settings, "key", _G._my_key)
-
+    S.obs_service_update(service, settings)
     S.obs_data_release(settings)
+    S.obs_frontend_save_streaming_service()
     return True
